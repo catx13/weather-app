94a43b19a8f8262767898b9248c2e8cf398d99e7
jasmitak <jasmita@lcscon.com>
2024-05-14 13:10:26 +0530

    adding shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import logo from './assets/weather_icon.png'
+import { Button } from './components/ui/button'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-background text-foreground'>
         <h1> Weather App </h1>
         <img src={logo} alt="" />
+        <Button>Login</Button>
        </div>
     </>
   )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import { Button } from '../components/ui/button'
+
+function Home() {
+
+  return (
+    <>
+      <div className='bg-background text-foreground'>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        The Book Club
+      </h1>
+        <Button>Login</Button>
+       </div>
+    </>
+  )
+}
+
+export default Home
