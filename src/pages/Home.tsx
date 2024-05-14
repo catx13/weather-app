12694aeb2164a979d17eca4b685964034269656e
jasmitak <jasmita@lcscon.com>
2024-05-14 13:17:04 +0530

    adding router package

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,16 @@
-import { Button } from '../components/ui/button'
+import { Button } from "../components/ui/button";
 
 function Home() {
-
   return (
     <>
       <div className='bg-background text-foreground'>
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        The Book Club
-      </h1>
+        <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+          The Book Club
+        </h1>
         <Button>Login</Button>
-       </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
