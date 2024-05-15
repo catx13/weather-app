65eaaf2c9265068cac093b202708c3c079ab8be1
jasmitak <jasmita@lcscon.com>
2024-05-15 09:43:57 +0530

    adding navbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,25 @@
+import { NavigationMenuDemo } from "@/appcomponents/NavigationBar";
 import { Button } from "../components/ui/button";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
 
 function Home() {
   return (
     <>
       <div className='bg-background text-foreground'>
+      
+        <NavigationMenuDemo />
         <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
-          The Book Club
+           Book Club
         </h1>
-        <Button>Login</Button>
       </div>
     </>
   );
