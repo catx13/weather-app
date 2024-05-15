8c3e60ef0a87de08a601b29076c2a4e7d6295369
jasmitak <jasmita@lcscon.com>
2024-05-15 16:52:24 +0530

    adding dashboard example

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,14 +1,25 @@
 import CreateAccountPage from "@/pages/CreateAccount";
+import { Dashboard } from "@/pages/Dashboard";
 import Home from "@/pages/Home";
+import Profile from "@/pages/Profile";
 import { createBrowserRouter } from "react-router-dom";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+
   },
   {
-    path:'signUp',
+    path:'/signUp',
     element:<CreateAccountPage/>
+  },
+  {
+    path:'/profile',
+    element:<Profile/>
+  },
+  {
+    path:'/dashboard',
+    element:<Dashboard/>
   }
 ]);
