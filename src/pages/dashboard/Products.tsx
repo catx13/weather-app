6fbeb7633e21a69d8dc9fff9597673b264b6ce90
jasmitak <jasmita@lcscon.com>
2024-05-16 12:52:07 +0530

    adding edit products page

--- src/pages/dashboard/Products.tsx
+++ src/pages/dashboard/Products.tsx
@@ -217,12 +217,13 @@ export function Products() {
                     Export
                   </span>
                 </Button>
-                <Button size="sm" className="h-8 gap-1">
+                <Link to={'/dashboard/products/edit'}>
+                <Button size="sm" className="h-8 gap-1" >
                   <PlusCircle className="h-3.5 w-3.5" />
                   <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
                     Add Product
                   </span>
-                </Button>
+                </Button></Link>
               </div>
             </div>
             <TabsContent value="all">
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import DashboardLayout from "@/layouts/DashboardLayout";
 import HomeLayout from "@/layouts/HomeLayout";
 import CreateAccountPage from "@/pages/CreateAccount";
 import DashboardPage from "@/pages/dashboard/Dashboard";
+import { EditProduct } from "@/pages/dashboard/EditProduct";
 import {  Orders } from "@/pages/dashboard/Orders";
 import { Products } from "@/pages/dashboard/Products";
 import Home from "@/pages/Home";
@@ -40,7 +41,12 @@ export const routes = createBrowserRouter([
           {
             path: "/dashboard/products",
             element: <Products/>,
+            children:[
+             
+            ]
           },
+          { path: "/dashboard/products/edit",
+          element: <EditProduct/>,}
     
     ]
   },
