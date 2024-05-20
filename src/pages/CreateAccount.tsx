5bf2fb012ed000baacf442fd0f28d5e2ab80684f
jasmitak <jasmita@lcscon.com>
2024-05-20 17:50:10 +0530

    folder deploy

--- src/pages/CreateAccount.tsx
+++
@@ -1,7 +0,0 @@
-import { CreateAccount } from '@/appcomponents/CreateAccount'
-
-export default function CreateAccountPage() {
-  return (
-    <div><CreateAccount/></div>
-  )
-}
---
+++ webapps-microbin-website/src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+      <div className='bg-background'>
+        <h1>Home Page</h1>
+      </div>
+    </>
+  );
+}
